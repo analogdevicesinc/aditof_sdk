5b58b93132e312a4abd3e2683bf01ad916ce70ca
Dan Nechita <dan.nechita@analog.com>
2019-05-24 17:07:45 +0300

    apps: uvc app - lower max packet size to obtain stability over USB
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- apps/uvc-app/config_pipe.sh
+++ apps/uvc-app/config_pipe.sh
@@ -6,7 +6,7 @@ sudo rmmod g_webcam
 sudo media-ctl -v -d /dev/media1 -l '"msm_csiphy1":1->"msm_csid0":0[1],"msm_csid0":1->"msm_ispif0":0[1],"msm_ispif0":1->"msm_vfe0_rdi0":0[1]'
 sudo media-ctl -v -d /dev/media1 -V '"addi903x 1-0064":0[fmt:SBGGR12/640x960 field:none],"msm_csiphy1":0[fmt:SBGGR12/640x960 field:none],"msm_csid0":0[fmt:SBGGR12/640x960 field:none],"msm_ispif0":0[fmt:SBGGR12/640x960 field:none],"msm_vfe0_rdi0":0[fmt:SBGGR12/640x960 field:none]'
 
-sudo modprobe g_webcam streaming_maxpacket=3072
+sudo modprobe g_webcam streaming_maxpacket=2048
 
 BASEDIR=$(dirname $0)
 
