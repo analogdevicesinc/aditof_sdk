68855ad36df9c114e24cf0b0340217583d2152f0
Daniel Guramulta <daniel.guramulta@analog.com>
2019-06-11 19:17:06 +0300

    sdk: include: add aditof.h header file
    Signed-off-by: Daniel Guramulta <daniel.guramulta@analog.com>

---
+++ sdk/include/aditof/aditof.h
@@ -0,0 +1,8 @@
+#include <aditof/camera.h>
+#include <aditof/camera_definitions.h>
+#include <aditof/device_interface.h>
+#include <aditof/frame.h>
+#include <aditof/frame_definitions.h>
+#include <aditof/frame_operations.h>
+#include <aditof/status_definitions.h>
+#include <aditof/system.h>
