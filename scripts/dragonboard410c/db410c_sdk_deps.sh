b0faa95c853c3b6bc114f9037a0a6e044d39fcd4
Andrei Cozma <andrei.cozma@analog.com>
2019-06-13 14:26:05 +0300

    Updated the db410c sdk deps install script

--- scripts/dragonboard410c/db410c_sdk_deps.sh
+++ scripts/dragonboard410c/db410c_sdk_deps.sh
@@ -8,6 +8,12 @@ sudo apt install v4l-utils -y
 sudo apt install libopencv-dev -y
 sudo apt install cmake -y
 
+#create the work folder
+rm -rf /home/linaro/workspace
+cd /home/linaro
+mkdir workspace && cd workspace
+mkdir github && cd github
+
 #install glog
 git clone https://github.com/google/glog
 cd glog
@@ -15,4 +21,25 @@ git checkout tags/v0.3.5
 mkdir build_0_3_5 && cd build_0_3_5
 cmake -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX=/opt/glog ..
 sudo cmake --build . --target install
-cd ../../
+
+#download and build the SDK
+cd /home/linaro/workspace/github
+git clone https://github.com/analogdevicesinc/aditof_sdk
+cd aditof_sdk
+mkdir build
+cd build
+cmake -DDRAGONBOARD=1 ..
+make -j8
+
+#build the uvc-app for host USB connectivity
+cd /home/linaro/workspace/github/aditof_sdk/apps/uvc-app
+make -j8
+
+#build and istall the USB daemon
+cd /home/linaro/workspace/github/aditof_sdk/apps/daemon
+sudo cp tof-programming.service /etc/systemd/system/
+mkdir build
+cd build
+cmake ..
+make -j8
+sudo systemctl enable tof-programming
