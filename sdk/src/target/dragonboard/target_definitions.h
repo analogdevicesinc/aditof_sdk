84d5382741a1f0feed0ddcd206bf1de8ee749c96
Dan Nechita <dan.nechita@analog.com>
2019-08-06 11:58:57 +0300

    sdk: Additional fixes for the local device
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- sdk/src/target/dragonboard/target_definitions.h
+++ sdk/src/target/dragonboard/target_definitions.h
@@ -1,10 +1,8 @@
 #ifndef TARGET_DEFINITIONS_H
 #define TARGET_DEFINITIONS_H
 
-#include <string>
+static const char *EEPROM_DEV_PATH = "/sys/bus/i2c/devices/0-0056/eeprom";
 
-static const std::string EEPROM_DEV_PATH = "/sys/bus/i2c/devices/0-0056/eeprom";
-
-static const std::string CAPTURE_DEVICE_NAME = "Qualcomm Camera Subsystem";
+static const char *CAPTURE_DEVICE_NAME = "Qualcomm Camera Subsystem";
 
 #endif // TARGET_DEFINITIONS_H
--- sdk/src/target/raspberrypi/target_definitions.h
+++ sdk/src/target/raspberrypi/target_definitions.h
@@ -1,10 +1,8 @@
 #ifndef TARGET_DEFINITIONS_H
 #define TARGET_DEFINITIONS_H
 
-#include <string>
+static const char *EEPROM_DEV_PATH = "/sys/bus/i2c/devices/1-0056/eeprom";
 
-static const std::string EEPROM_DEV_PATH = "/sys/bus/i2c/devices/1-0056/eeprom";
-
-static const std::string CAPTURE_DEVICE_NAME = "unicam";
+static const char *CAPTURE_DEVICE_NAME = "unicam";
 
 #endif // TARGET_DEFINITIONS_H
