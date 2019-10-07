2f430f13c6ea1ff7b33817f34eb0beed8f047e90
Daniel Guramulta <daniel.guramulta@analog.com>
2019-10-07 10:40:15 +0300

    scripts: linux: refactor code and also install OpenCV 3.4.1
    Signed-off-by: Daniel Guramulta <daniel.guramulta@analog.com>

--- scripts/linux/setup.sh
+++ scripts/linux/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+source_dir=$(cd "$(dirname "$0")/../.."; pwd)
+
+. "${source_dir}"/ci/travis/lib.sh
+
 print_help() {
         echo "./setup [OPTIONS]"
         echo ""
@@ -19,68 +23,8 @@ print_help() {
 }
 
 install_required_packages() {
-        sudo apt-get -y install cmake libssl-dev libopencv-dev v4l-utils
-}
-
-get_deps_source_code() {
-    CLONE_DIRECTORY=$1
-    pushd "${CLONE_DIRECTORY}"
-
-    [ -d "glog" ] || {
-       git clone --branch v0.3.5 --depth 1 https://github.com/google/glog
-    }
-
-    [ -d "protobuf" ] || {
-       git clone --branch v3.9.0 --depth 1 https://github.com/protocolbuffers/protobuf
-    }
-
-    [ -d "libwebsockets" ] || {
-       git clone --branch v3.1-stable --depth 1 https://github.com/warmcat/libwebsockets
-    }
-
-    popd
-}
-
-build_and_install_glog() {
-    REPO_DIR=$1
-    INSTALL_DIR=$2
-    BUILD_DIR=${REPO_DIR}/build_0_3_5
-    NUM_JOBS=$3
-
-    mkdir -p ${BUILD_DIR}
-    pushd ${BUILD_DIR}
-    cmake .. -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-    make -j ${NUM_JOBS}
-    sudo make install
-    popd
-}
-
-build_and_install_protobuf() {
-    REPO_DIR=$1
-    INSTALL_DIR=$2
-    BUILD_DIR=${REPO_DIR}/build_3_9_0
-    NUM_JOBS=$3
-
-    mkdir -p ${BUILD_DIR}
-    pushd ${BUILD_DIR}
-    cmake ../cmake/ -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-    make -j ${NUM_JOBS}
-    sudo make install
-    popd
-}
-
-build_and_install_websockets() {
-    REPO_DIR=$1
-    INSTALL_DIR=$2
-    BUILD_DIR=${REPO_DIR}/build_3_1_0
-    NUM_JOBS=$3
-
-    mkdir -p ${BUILD_DIR}
-    pushd ${BUILD_DIR}
-    cmake .. -DLWS_STATIC_PIC=ON -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-    make -j ${NUM_JOBS}
-    sudo make install
-    popd
+        sudo apt install -y build-essential cmake python-dev python3-dev \
+        libssl-dev git
 }
 
 yes_or_exit() {
@@ -177,15 +121,17 @@ setup() {
         deps_install_dir=$(pwd)
         popd
 
+        CMAKE_OPTIONS="-DWITH_PYTHON=on -DWITH_OPENCV=on"
 
         get_deps_source_code ${deps_dir}
 
-        build_and_install_glog ${deps_dir}/glog ${deps_install_dir}/glog ${NUM_JOBS}
-        build_and_install_protobuf ${deps_dir}/protobuf ${deps_install_dir}/protobuf ${NUM_JOBS}
-        build_and_install_websockets ${deps_dir}/libwebsockets ${deps_install_dir}/websockets ${NUM_JOBS}
+        build_and_install_glog ${deps_dir}/glog ${deps_install_dir}/glog
+        build_and_install_protobuf ${deps_dir}/protobuf ${deps_install_dir}/protobuf
+        build_and_install_websockets ${deps_dir}/libwebsockets ${deps_install_dir}/websockets
+        build_and_install_opencv ${deps_dir}/"opencv-${OPENCV}" ${deps_install_dir}/"opencv-${OPENCV}"
 
         pushd "${build_dir}"
-        cmake "${source_dir}" -DWITH_PYTHON=on -DWITH_OPENCV=on -DCMAKE_PREFIX_PATH="${deps_install_dir}/glog;${deps_install_dir}/protobuf;${deps_install_dir}/websockets"
+        cmake "${source_dir}" "${CMAKE_OPTIONS}" -DCMAKE_PREFIX_PATH="${deps_install_dir}/glog;${deps_install_dir}/protobuf;${deps_install_dir}/websockets;${deps_install_dir}/opencv-${OPENCV}"
         make -j ${NUM_JOBS}
 }
 
