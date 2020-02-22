13ba9efdaed022890fecdf208416441be9a7f719
acozma <andrei.cozma@analog.com>
2020-02-22 02:48:29 +0200

    Updated the adaptor example use scripts

--- bindings/matlab/README.md
+++ bindings/matlab/README.md
@@ -1,10 +1,16 @@
-# 3D Time of Flight : Matlab bindings
+# MATLAB Image Acquisition Toolbox adaptor
 
 ### Overview
-The SDK provides bindings for matlab through the help of a Custom Adaptor which facilitates the use of the SDK with the Image Acquisition Toolbox from Matlab.
+The SDK provides bindings for MATLAB through the help of a Custom Adaptor which facilitates the use of the SDK with the Image Acquisition Toolbox. More info about custom adaptors for the Image Acquisition Toolbox can be found [here](www.mathworks.com/help/imaq/creating-custom-adaptors.html).
 
-### Building the bindings
+### Building and using the adaptor
 
-When building the bindings, specify the cmake option `-DWITH_MATLAB=on`. If Matlab is not found automatically by cmake, the option `-DMatlab_ROOT_DIR=<path>` should be used to specifiy the path to the matlab installation folder.
+When building the adaptor, specify the cmake option `-DWITH_MATLAB=on`. If MATLAB is not found automatically by cmake, the option `-DMatlab_ROOT_DIR=<path>` should be used to specifiy the path to the matlab installation folder.
 
-When using the `test_load.m` file, `imaqregister` might return an error that sound like this `...aditofadapter.dll is not a valid adaptor`. To fix this issue make sure that `aditof.dll` is in the system environment variable `PATH`.
+The output of the build is a dymanic library (.so for Linux and .dll for Windows) that is to be loaded in MATLAB by using the Image Acquisition Toolbox specific functions. The `aditof_imaq.m` script provides a full example on how to use the custom adaptor in MATLAB. Before running the `aditof_imaq.m` script make sure to copy the aditof.so/dll and the aditofadapter.so/dll to the folder where the script is.
+
+The adaptor binaries for Linux and Windows are provided as part of the [SDK releases](https://github.com/analogdevicesinc/aditof_sdk/releases/latest).
+
+### Troubleshooting
+
+When using the `aditof_imaq.m` file, `imaqregister` might return an error that sound like this `...aditofadapter.dll is not a valid adaptor`. To fix this issue make sure that `aditof.dll` is in the system environment variable `PATH`.
--- bindings/matlab/aditof_imaq.m
+++ bindings/matlab/aditof_imaq.m
@@ -1,31 +1,63 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Load the adaptor
-adapts = imaqregister([pwd, '\aditofadapter.dll']); %when running on Linux load the aditofadapter.so
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+imaqregister([pwd, '\aditofadapter.dll']); % when running on Linux load the aditofadapter.so
 imaqreset;
-
-%%%% Display information about the adaptor
 imaqhwinfo
-imaqfind
-a = imaqhwinfo('aditofadapter')
-a.DeviceInfo
-a.DeviceInfo.DeviceName
-clear a
 
-%%%% Start a video stream using either USB or Ethernet
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Connect to the camera using either USB or Ethernet
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% USB connection
 vid = videoinput('aditofadapter');
-
 %% Ethernet connection
-%vid = videoinput('aditofadapter', 1, "<ip>"); % Replace the ip with the device on which the server runs
+%vid = videoinput('aditofadapter', 1, "<ip>"); % replace <ip> with the IP of the camera device 
 
-preview(vid);
-pause
-vid = [];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Set the camera to output Depth and the mode to Near range
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+src = getselectedsource(vid); 
+src.FrameType = ‘Depth’;
+src. CameraMode = ‘Near’;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Play the depth video stream for a number of frames
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+start(vid);
+pause(1);               
+for i = 1:300              
+    depthMap = getsnapshot(vid);
+    figure(1); imshow(depthMap);
+end
+stop(vid);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Set the camera to output IR and the mode to Medium range
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+src = getselectedsource(vid); 
+src.FrameType = ‘Ir’;
+src. CameraMode = ‘Medium’;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Play the IR video stream for number of frames
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+start(vid);
+pause(1);               
+for i = 1:300              
+    depthMap = getsnapshot(vid);
+    figure(2); imshow(depthMap);
+end
+stop(vid);
+delete(vid);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Run the image acquisition tool to see all the adaptor's attributes and play the video
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 imaqtool;
 pause;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Unregister the adaptor
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 imaqregister([pwd, '\aditofadapter.dll'],'unregister');
-imaqreset
-imaqhwinfo
+imaqreset;
