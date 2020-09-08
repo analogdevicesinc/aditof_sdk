f39fbec8051b26ba9c5b27c72a925552441654aa
Virgil Litan <virgil.litan@analog.com>
2020-09-08 10:59:27 +0300

    remove unused members from controller
    Signed-off-by: Virgil Litan <virgil.litan@analog.com>

--- tools/eeprom-tool/eepromtoolcontroller.h
+++ tools/eeprom-tool/eepromtoolcontroller.h
@@ -56,13 +56,11 @@ class EepromToolController {
     aditof::Status writeFileToEeprom(char const* filename);
     aditof::Status readEepromToFile(char const* filename);
 
-    aditof::Status checkData(const std::vector<uint8_t> data);
     aditof::Status writeEeprom(const std::vector<uint8_t> data);
     aditof::Status readEeprom(std::vector<uint8_t>& data);
     
     aditof::Status readFile(char const* filename, std::vector<uint8_t>&);
     aditof::Status writeFile(char const* filename, const std::vector<uint8_t>);
-    aditof::Status setCameraRevision(const std::string &revision);
   private:
   private:
     aditof::System *m_system;
@@ -70,9 +68,6 @@ class EepromToolController {
 
     std::shared_ptr<aditof::EepromInterface> m_eeprom;
     std::shared_ptr<aditof::DeviceInterface> m_device;
-    int m_eepromInUse;
-
-   bool m_IsEthernetConnection = false;
 };
 
 #endif
