424558638ae2d7538cc765d9e72065ba3761d7c0
Virgil Litan <virgil.litan@analog.com>
2020-10-13 16:53:17 +0300

    add xaviernx target
    Signed-off-by: Virgil Litan <virgil.litan@analog.com>

---
+++ sdk/src/connections/mipi/xaviernx/target_definitions.h
@@ -0,0 +1,48 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, Analog Devices, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef TARGET_DEFINITIONS_H
+#define TARGET_DEFINITIONS_H
+
+static const char *TEMP_SENSOR_DEV_PATH = "/dev/i2c-9";
+static const char *EEPROM_DEV_PATH = "/sys/bus/i2c/devices/9-0056/eeprom";
+
+static const char *EEPROM_REPLACEMENT_PATH =
+    "/home/analog/workspace/github/aditof_sdk/chicony_firmware.bin";
+
+static const char *TEMP_SENSOR_REPLACEMENT_DEV_PATH = "";
+
+static const char *CAPTURE_DEVICE_NAME =
+    "vi-output, addi9036 9-0064|vi-output, addi9036 10-0064";
+
+static const int NUM_VIDEO_DEVS = 2;
+
+#endif // TARGET_DEFINITIONS_H
