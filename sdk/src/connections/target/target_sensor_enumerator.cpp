7ba0c22455267a90fe5ef1eeca6c5e01dda62acf
Dan Nechita <dan.nechita@analog.com>
2020-11-25 12:14:01 +0200

    sdk: Rename connections/mipi to connections/target
    'Target' covers more types of connections. Depth sensor is connected through MIPI interface but other sensors can have I2C connections (e.g. temperature sensor).
    
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- sdk/src/connections/target/target_sensor_enumerator.cpp
+++ sdk/src/connections/target/target_sensor_enumerator.cpp
@@ -29,7 +29,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#include "connections/mipi/target_sensor_enumerator.h"
+#include "connections/target/target_sensor_enumerator.h"
 
 using namespace aditof;
 
