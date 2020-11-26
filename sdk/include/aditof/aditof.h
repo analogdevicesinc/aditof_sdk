5f6ec6c95871b17fcae001efa5174691943930e8
Dan Nechita <dan.nechita@analog.com>
2020-11-26 17:05:50 +0200

    sdk: Update cameras and system to the new changes in low level API
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- sdk/include/aditof/aditof.h
+++ sdk/include/aditof/aditof.h
@@ -31,11 +31,12 @@
  */
 #include <aditof/camera.h>
 #include <aditof/camera_definitions.h>
-#include <aditof/device_interface.h>
-#include <aditof/eeprom_interface.h>
+#include <aditof/depth_sensor_interface.h>
 #include <aditof/frame.h>
 #include <aditof/frame_definitions.h>
 #include <aditof/frame_operations.h>
 #include <aditof/status_definitions.h>
+#include <aditof/storage_interface.h>
 #include <aditof/system.h>
+#include <aditof/temperature_sensor_interface.h>
 #include <aditof/version.h>
