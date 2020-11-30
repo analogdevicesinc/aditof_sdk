70ec7eecab8759771779d3741d03837ba35dd2d5
Dan Nechita <dan.nechita@analog.com>
2020-11-30 17:20:55 +0200

    sdk: SensorEnumerator on Network creates Network wrapper for tmp sensors
    tmp sensors = temperature sensors
    
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

---
+++ sdk/src/connections/network/network_temperature_sensor.cpp
@@ -0,0 +1,174 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, Analog Devices, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "connections/network/network_temperature_sensor.h"
+#include "connections/network/network.h"
+
+#include <glog/logging.h>
+
+using namespace aditof;
+
+struct NetworkTemperatureSensor::ImplData {
+    EthernetHandle *handle;
+    std::string name;
+};
+
+NetworkTemperatureSensor::NetworkTemperatureSensor(const std::string &name)
+    : m_implData(new ImplData) {
+    m_implData->name = name;
+}
+
+NetworkTemperatureSensor::~NetworkTemperatureSensor() = default;
+
+Status NetworkTemperatureSensor::open(void *handle) {
+    if (!handle) {
+        LOG(ERROR) << "Invalid handle";
+        return Status::INVALID_ARGUMENT;
+    }
+
+    m_implData->handle = reinterpret_cast<struct EthernetHandle *>(handle);
+
+    Network *net = m_implData->handle->net;
+    std::unique_lock<std::mutex> mutex_lock(m_implData->handle->net_mutex);
+
+    if (!net->isServer_Connected()) {
+        LOG(WARNING) << "Not connected to server";
+        return Status::UNREACHABLE;
+    }
+
+    net->send_buff.set_func_name("TemperatureSensorOpen");
+    net->send_buff.mutable_device_data()->add_temperature_sensors(
+        m_implData->name);
+    net->send_buff.set_expect_reply(true);
+
+    if (net->SendCommand() != 0) {
+        LOG(WARNING) << "Send Command Failed";
+        return Status::INVALID_ARGUMENT;
+    }
+
+    if (net->recv_server_data() != 0) {
+        LOG(WARNING) << "Receive Data Failed";
+        return Status::GENERIC_ERROR;
+    }
+
+    if (net->recv_buff.server_status() !=
+        payload::ServerStatus::REQUEST_ACCEPTED) {
+        LOG(WARNING) << "API execution on Target Failed";
+        return Status::GENERIC_ERROR;
+    }
+
+    Status status = static_cast<Status>(net->recv_buff.status());
+
+    return status;
+}
+
+Status NetworkTemperatureSensor::read(float &temperature) {
+    Network *net = m_implData->handle->net;
+    std::unique_lock<std::mutex> mutex_lock(m_implData->handle->net_mutex);
+
+    if (!net->isServer_Connected()) {
+        LOG(WARNING) << "Not connected to server";
+        return Status::UNREACHABLE;
+    }
+
+    net->send_buff.set_func_name("TemperatureSensorRead");
+    net->send_buff.mutable_device_data()->add_temperature_sensors(
+        m_implData->name);
+    net->send_buff.set_expect_reply(true);
+
+    if (net->SendCommand() != 0) {
+        LOG(WARNING) << "Send Command Failed";
+        return Status::INVALID_ARGUMENT;
+    }
+
+    if (net->recv_server_data() != 0) {
+        LOG(WARNING) << "Receive Data Failed";
+        return Status::GENERIC_ERROR;
+    }
+
+    if (net->recv_buff.server_status() !=
+        payload::ServerStatus::REQUEST_ACCEPTED) {
+        LOG(WARNING) << "API execution on Target Failed";
+        return Status::GENERIC_ERROR;
+    }
+
+    Status status = static_cast<Status>(net->recv_buff.status());
+
+    if (status == Status::OK) {
+        temperature = net->recv_buff.float_payload(0);
+    }
+
+    return status;
+}
+
+Status NetworkTemperatureSensor::close() {
+    Network *net = m_implData->handle->net;
+    std::unique_lock<std::mutex> mutex_lock(m_implData->handle->net_mutex);
+
+    if (!net->isServer_Connected()) {
+        LOG(WARNING) << "Not connected to server";
+        return Status::UNREACHABLE;
+    }
+
+    net->send_buff.set_func_name("TemperatureSensorClose");
+    net->send_buff.mutable_device_data()->add_temperature_sensors(
+        m_implData->name);
+    net->send_buff.set_expect_reply(true);
+
+    if (net->SendCommand() != 0) {
+        LOG(WARNING) << "Send Command Failed";
+        return Status::INVALID_ARGUMENT;
+    }
+
+    if (net->recv_server_data() != 0) {
+        LOG(WARNING) << "Receive Data Failed";
+        return Status::GENERIC_ERROR;
+    }
+
+    if (net->recv_buff.server_status() !=
+        payload::ServerStatus::REQUEST_ACCEPTED) {
+        LOG(WARNING) << "API execution on Target Failed";
+        return Status::GENERIC_ERROR;
+    }
+
+    Status status = static_cast<Status>(net->recv_buff.status());
+
+    if (status == Status::OK) {
+        m_implData->handle = nullptr;
+    }
+
+    return status;
+}
+
+Status NetworkTemperatureSensor::getName(std::string &name) const {
+    name = m_implData->name;
+    return Status::OK;
+}
