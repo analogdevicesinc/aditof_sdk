eb7090f79f042c9a4466b7a4a2dde759c59098fe
Dan Nechita <dan.nechita@analog.com>
2020-12-03 12:25:19 +0200

    sdk: Implement TargetSensorEnumerator on Raspberry Pi
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- sdk/src/connections/target/raspberrypi/sensor_enumerator_raspberrypi.cpp
+++ sdk/src/connections/target/raspberrypi/sensor_enumerator_raspberrypi.cpp
@@ -43,62 +43,16 @@
 
 using namespace aditof;
 
-//aditof::Status DeviceEnumeratorImpl::findDevices(
-//    std::vector<aditof::DeviceConstructionData> &devices) {
-//    using namespace aditof;
-//    Status status = Status::OK;
-
-//    LOG(INFO) << "Looking for devices on the target";
-
-//    // TO DO: Do we still need to do this?
-//    // Find all video device paths
-//    std::vector<std::string> videoPaths;
-//    const std::string videoDirPath("/dev/");
-//    const std::string videoBaseName("video");
-
-//    DIR *dirp = opendir(videoDirPath.c_str());
-//    struct dirent *dp;
-//    while ((dp = readdir(dirp))) {
-//        if (!strncmp(dp->d_name, videoBaseName.c_str(),
-//                     videoBaseName.length())) {
-//            std::string fullvideoPath = videoDirPath + std::string(dp->d_name);
-//            videoPaths.emplace_back(fullvideoPath);
-//        }
-//    }
-//    closedir(dirp);
-
-//    for (const auto &video : videoPaths) {
-//        std::string devPath;
-
-//        if (devPath.empty()) {
-//            continue;
-//        }
-//    }
-//    // TO DO: Don't guess the device, find a way to identify it so we are sure
-//    // we've got the right device and is compatible with the SDK
-//    DeviceConstructionData devData;
-//    devData.connectionType = ConnectionType::LOCAL;
-//    devData.driverPath = "/dev/video0";
-
-//    // Check if EEPROM is available
-//    struct stat st;
-//    if (stat(EEPROM_DEV_PATH, &st) == 0) {
-//        EepromConstructionData eData;
-//        eData.driverName = EEPROM_NAME;
-//        eData.driverPath = EEPROM_DEV_PATH;
-//        devData.eeproms.emplace_back(eData);
-//    }
-
-//    devices.emplace_back(devData);
-
-//    DLOG(INFO) << "Looking at: " << devData.driverPath
-//               << " for an eligible TOF camera";
-
-//    return status;
-//}
-
 Status TargetSensorEnumerator::searchSensors() {
-    // TO DO: implement this
+
+    LOG(INFO) << "Looking for sensors on the target: Raspberry PI";
+
+    // TO DO: Don't guess the device, find a way to identify it so we are sure
+    // we've got the right sensor and it's compatible with the SDK
+    SensorInfo sInfo;
+    sInfo.sensorType = SensorType::SENSOR_ADDI9036;
+    sInfo.driverPath = "/dev/video0";
+    m_sensorsInfo.emplace_back(sInfo);
 
     return Status::OK;
 }
