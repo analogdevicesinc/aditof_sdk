33547b357fc6060bd2dee6572bcc6c8ecd54e2c5
Virgil Litan <virgil.litan@analog.com>
2020-12-14 18:05:44 +0200

    use fullData sizes in toybrick deinterleaving
    Signed-off-by: Virgil Litan <virgil.litan@analog.com>

--- sdk/src/connections/mipi/local_device.cpp
+++ sdk/src/connections/mipi/local_device.cpp
@@ -515,7 +515,8 @@ aditof::Status LocalDevice::getFrame(uint16_t *buffer) {
 
     unsigned int width;
     unsigned int height;
-    unsigned int buf_data_len;
+    unsigned int fullDataHeight = m_implData->frameDetails.fullDataHeight;
+    unsigned int fullDataWidth = m_implData->frameDetails.fullDataWidth;
     uint8_t *pdata;
 
     width = m_implData->frameDetails.width;
@@ -559,16 +560,16 @@ aditof::Status LocalDevice::getFrame(uint16_t *buffer) {
         if (m_implData->frameDetails.type == "depth_only") {
             memcpy(buffer, pdata, buf.bytesused);
         } else if (m_implData->frameDetails.type == "ir_only") {
-            memcpy(buffer + (width * height) / 2, pdata, buf.bytesused);
+            memcpy(buffer + (width * height), pdata, buf.bytesused);
         } else {
-            uint32_t j = 0, j1 = width * height / 2;
-            for (uint32_t i = 0; i < height; i += 2) {
+            uint32_t j = 0, j1 = width * height;
+            for (uint32_t i = 0; i < fullDataHeight; i += 2) {
                 memcpy(buffer + j, pdata + i * width * 2, width * 2);
                 j += width;
                 memcpy(buffer + j1, pdata + (i + 1) * width * 2, width * 2);
                 j1 += width;
             }
-            for (uint32_t i = 0; i < width * height; i += 2) {
+            for (uint32_t i = 0; i < fullDataWidth * fullDataHeight; i += 2) {
                 buffer[i] =
                     ((buffer[i] & 0x00FF) << 4) | ((buffer[i]) & 0xF000) >> 12;
                 buffer[i + 1] = ((buffer[i + 1] & 0x00FF) << 4) |
