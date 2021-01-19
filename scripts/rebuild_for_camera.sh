5d29a48cb47197530480f8def8d54f59b018ee34
SeptimiuVana <Vana.Septimiu@analog.com>
2021-01-19 10:21:32 +0200

    Added descriprion for rebuild_for_camera.sh script
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- scripts/rebuild_for_camera.sh
+++ scripts/rebuild_for_camera.sh
@@ -1,3 +1,11 @@
+#This script can be used to rebuild the SDK depending on various platforms and cameras.
+#Its purpose is to be called from a shortcut placed on desktop.
+#It expects the parameters passed as the CMAKE command.
+
+#Possible command for the camera: -DUSE_FXTOF1=1 
+#Possible commands for the board: -DDRAGONBOARD=1 -DRASPBERRYPI=1 -DJETSON=1 -DXAVIERNX=1 -DXAVIER=1
+#The implicit build, if no parameters are passed is for AD-96TOF1-EBZ build on linux.
+
 #!/bin/bash
 
 cd $(dirname $(dirname $(realpath $0)))
