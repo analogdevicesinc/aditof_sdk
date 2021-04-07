8a4dc39e56ffd6d305850ebcef43554a871624ad
Andrei Cozma <andrei.cozma@analog.com>
2021-04-07 23:16:31 +0300

    Fix geometry calibration issue
    Fixing an issue introduced in this commit: https://github.com/analogdevicesinc/aditof_sdk/commit/7a7aadcb780afc585a0becee40f9d55c434ee386#diff-900b0798abcd7bc98839e2c722f282a36bca1ae4fd110e5184b0155de5900e9b

--- sdk/src/cameras/ad-96tof1-ebz/calibration_96tof1.cpp
+++ sdk/src/cameras/ad-96tof1-ebz/calibration_96tof1.cpp
@@ -497,7 +497,7 @@ void Calibration96Tof1::buildGeometryCalibrationCache(
                 double tanYAngle = (y0 - i) / fy;
 
                 m_geometry_cache[i * width + j] =
-                    sqrt(1 + tanXAngle * tanXAngle + tanYAngle * tanYAngle);
+                1.0 / sqrt(1 + tanXAngle * tanXAngle + tanYAngle * tanYAngle);
             } else {
                 m_geometry_cache[i * width + j] = 1;
             }
