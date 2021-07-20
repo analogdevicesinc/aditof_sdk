9fe20f14a7d20a8e8e8705838f8fc338cfbadac7
SeptimiuVana <Vana.Septimiu@analog.com>
2021-07-20 16:24:22 +0300

    network_depth_sensor.cpp: enable/disable deinterleave depending on data received
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- sdk/src/connections/network/network_depth_sensor.cpp
+++ sdk/src/connections/network/network_depth_sensor.cpp
@@ -389,10 +389,17 @@ aditof::Status NetworkDepthSensor::getFrame(uint16_t *buffer) {
     // throughput (raw data chunck is smaller, deinterleaving is usually slower
     // on target).
 
-    aditof::deinterleave(net->recv_buff.bytes_payload(0).c_str(), buffer,
-                         net->recv_buff.bytes_payload(0).length(),
-                         m_implData->frameDetails_cache.fullDataWidth,
-                         m_implData->frameDetails_cache.fullDataHeight);
+    // net->recv_buff.bytes_payload(0).c_str() is 1 if deinterleaving is needed
+    // or 0 if not.
+    if(net->recv_buff.int32_payload(0) == 0) {
+        memcpy(buffer, net->recv_buff.bytes_payload(0).c_str(),
+                net->recv_buff.bytes_payload(0).length());
+    } else {
+        aditof::deinterleave(net->recv_buff.bytes_payload(0).c_str(), buffer,
+                             net->recv_buff.bytes_payload(0).length(),
+                             m_implData->frameDetails_cache.fullDataWidth,
+                             m_implData->frameDetails_cache.fullDataHeight);
+    }
 
     return status;
 }
