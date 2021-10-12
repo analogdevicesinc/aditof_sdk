a3bf389cba4035d9af888f5951fda0b7e8a32ba2
rbudai98 <robert.budai@analog.com>
2021-10-12 08:25:18 +0300

    frame_impl: Formatted and rewritten into original form
    Signed-off-by: rbudai98 <robert.budai@analog.com>

--- sdk/src/frame_impl.cpp
+++ sdk/src/frame_impl.cpp
@@ -133,7 +133,7 @@ aditof::Status FrameImpl::getData(aditof::FrameDataType dataType,
 void FrameImpl::allocFrameData(const aditof::FrameDetails &details) {
 
     m_fullData = new uint16_t[details.width * details.height * 2 +
-                              (int)(details.rgbWidth * details.rgbHeight*1.5)];
+                              details.rgbWidth * details.rgbHeight];
     m_depthData = m_fullData;
     m_irData = m_fullData + (details.width * details.height);
     m_rgbData = m_fullData + (details.width * details.height * 2);
