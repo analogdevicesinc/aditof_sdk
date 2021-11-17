8be282d7ea201c4adb70d3891f816a3b1ebf3796
SeptimiuVana <Vana.Septimiu@analog.com>
2021-11-17 17:16:34 +0200

    examples/aditof-demo: display point cloud with red points
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- examples/aditof-demo/cvui.h
+++ examples/aditof-demo/cvui.h
@@ -66,6 +66,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/viz.hpp>
+#include <opencv2/viz/viz3d.hpp>
 
 namespace cvui {
 /**
