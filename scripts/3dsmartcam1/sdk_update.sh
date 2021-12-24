3e290f0ab592e175b06a81ae3a6c1c3661c8ca20
SeptimiuVana <Vana.Septimiu@analog.com>
2021-12-24 13:43:54 +0200

    scripts/3dsmartcam1/sdk_update.sh: add fetch command before rebuilding
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- scripts/3dsmartcam1/sdk_update.sh
+++ scripts/3dsmartcam1/sdk_update.sh
@@ -3,21 +3,27 @@
 cd ~/Workspace/aditof_sdk/deps
 
 cd glog
-sudo rm -rf build_0_3_5
+git fetch --all --tags
+git checkout tags/v0.3.5
+sudo rm -rf build_0_3_5 || true
 mkdir build_0_3_5 && cd build_0_3_5
 cmake -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX=/opt/glog ..
 sudo make -j8 && sudo make install
 cd ../..
 
 cd libwebsockets
-sudo rm -rf build_3_2_3
+git fetch --all --tags
+git checkout tags/v3.2.3
+sudo rm -rf build_3_2_3 || true
 mkdir build_3_2_3 && cd build_3_2_3
 cmake -DLWS_STATIC_PIC=ON -DCMAKE_INSTALL_PREFIX=/opt/websockets ..
 sudo make -j8 && sudo make install
 cd ../..
 
 cd protobuf
-sudo rm -rf build_3_9_0
+git fetch --all --tags
+git checkout tags/v3.9.0
+sudo rm -rf build_3_9_0 || true
 mkdir build_3_9_0 && cd build_3_9_0
 cmake -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_PREFIX=/opt/protobuf ../cmake
 sudo make -j8 && sudo make install
@@ -26,7 +32,7 @@ cd ../..
 cd ~/Workspace/aditof_sdk/
 git pull
 git fetch
-sudo rm -rf build
+sudo rm -rf build || true
 mkdir build && cd build
 cmake -DUSE_3D_SMART=1 -DCMAKE_PREFIX_PATH="/opt/glog;/opt/protobuf;/opt/websockets" ..
 make -j8
