28da47d3532aff1412d9d76ed2c2ee9680865138
SeptimiuVana <Vana.Septimiu@analog.com>
2022-01-25 14:57:10 +0200

    azure-pipelines.yml: add cache for deps
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- ci/azure/windows-build/install_deps.ps1
+++ ci/azure/windows-build/install_deps.ps1
@@ -2,7 +2,7 @@ $ARCH=$Env:ARCH
 $GENERATOR=$Env:COMPILER
 
 $local_path=$pwd
-mkdir deps
+mkdir deps_installed
 mkdir -p deps_installed/glog
 mkdir -p deps_installed/protobuf 
 mkdir -p deps_installed/websockets
@@ -11,16 +11,15 @@ git clone --branch v0.3.5 --depth 1 https://github.com/google/glog
 cd glog
 mkdir build_0_3_5 
 cd build_0_3_5
-cmake -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX=$local_path/deps_installed/glog -G $GENERATOR ..
+cmake -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/glog" -G $GENERATOR ..
 cmake --build . --target install -j 4
 
 cd $local_path
-
 git clone --branch v3.2.3 --depth 1 https://github.com/warmcat/libwebsockets
 cd libwebsockets
 mkdir build_3_2_3
 cd build_3_2_3
-cmake -DLWS_WITH_SSL=OFF -DLWS_WITHOUT_BUILTIN_SHA1=OFF -DCMAKE_INSTALL_PREFIX=./local_path/websockets -G $GENERATOR ..
+cmake -DLWS_WITH_SSL=OFF -DLWS_WITHOUT_BUILTIN_SHA1=OFF -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/websockets" -G $GENERATOR ..
 cmake --build . --target install -j4
 
 cd $local_path
@@ -28,7 +27,7 @@ git clone --branch v3.9.0 --depth 1 https://github.com/protocolbuffers/protobuf
 cd protobuf
 mkdir build_3_9_0 
 cd build_3_9_0
-cmake -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -DCMAKE_INSTALL_PREFIX=./local_path/protobuf -G $GENERATOR ../cmake
+cmake -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/protobuf" -G $GENERATOR ../cmake
 cmake --build . --target install -j4
 
 cd $local_path
