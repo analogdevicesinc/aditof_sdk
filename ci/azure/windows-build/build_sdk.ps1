db3b0938483db3d0944fe6826569f14f3bbbf728
SeptimiuVana <Vana.Septimiu@analog.com>
2022-01-26 16:40:21 +0200

    ci/azure: fix windows opencv path
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- ci/azure/windows-build/build_sdk.ps1
+++ ci/azure/windows-build/build_sdk.ps1
@@ -4,5 +4,5 @@ cd ..
 
 mkdir build
 cd build
-cmake -DCMAKE_PREFIX_PATH="../deps_installed/glog;../deps_installed/protobuf;../deps_installed/websockets" -DOpenCV_DIR="..\deps_installed\opencv\build\x64\vc15\lib" -DOPENSSL_INCLUDE_DIRS="..deps_installed\OpenSSL-Win64\include" ..
+cmake -DCMAKE_PREFIX_PATH="../deps_installed/glog;../deps_installed/protobuf;../deps_installed/websockets" -DOpenCV_DIR="../deps_installed/opencv/build/x64/vc15/lib" -DOpenCV_INCLUDE_DIR="../deps_installed/opencv/build/include" -DOPENSSL_INCLUDE_DIRS="../deps_installed/OpenSSL-Win64/include" ..
 cmake --build . --target install --config Release -j 4
