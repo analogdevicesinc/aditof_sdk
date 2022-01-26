7cffe591ada1596513122e1870527260ee5f8610
SeptimiuVana <Vana.Septimiu@analog.com>
2022-01-26 17:10:08 +0200

    ci/azure: cache deps
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- ci/azure/windows-build/build_sdk.ps1
+++ ci/azure/windows-build/build_sdk.ps1
@@ -1,8 +1,8 @@
-cd deps_installed
-tree
-cd ..
-
-mkdir build
-cd build
-cmake -DCMAKE_PREFIX_PATH="../deps_installed/glog;../deps_installed/protobuf;../deps_installed/websockets" -DOpenCV_DIR="../deps_installed/opencv/build/x64/vc15/lib" -DOpenCV_INCLUDE_DIR="../deps_installed/opencv/build/include" -DOPENSSL_INCLUDE_DIRS="../deps_installed/OpenSSL-Win64/include" ..
-cmake --build . --target install --config Release -j 4
+#cd deps_installed
+#tree
+#cd ..
+#
+#mkdir build
+#cd build
+#cmake -DCMAKE_PREFIX_PATH="../deps_installed/glog;../deps_installed/protobuf;../deps_installed/websockets;../deps_installed/opencv/build" -DOpenCV_DIR="../deps_installed/opencv/build/x64/vc15/lib" -DOpenCV_INCLUDE_DIR="../deps_installed/opencv/build/include" -DOPENSSL_INCLUDE_DIRS="../deps_installed/OpenSSL-Win64/include" ..
+#cmake --build . --target install --config Release -j 4
