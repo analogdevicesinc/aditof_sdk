24ea2ccf0ab0ab03f26f6a5fa8c3dd37a475b50c
SeptimiuVana <Vana.Septimiu@analog.com>
2022-01-27 16:32:14 +0200

    ci/azure: generate and add debug binaries
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- ci/azure/windows-build/build_sdk.ps1
+++ ci/azure/windows-build/build_sdk.ps1
@@ -2,25 +2,43 @@
 cinst opencv --version 3.4.1
 
 #build sdk
-mkdir build
-cd build
-cmake -DWITH_OPENCV=on -DWITH_PYTHON=on -DCMAKE_PREFIX_PATH="../deps_installed/glog;../deps_installed/protobuf;../deps_installed/websockets;..\deps_installed\OpenSSL-Win64" -DOpenCV_DIR="C:/tools/opencv/build/x64/vc15/lib" -DOPENSSL_INCLUDE_DIRS="..\deps_installed\OpenSSL-Win64\include" ..
+mkdir build_Release
+mkdir build_Debug
+
+cd build_Release
+cmake -DWITH_OPENCV=on -DWITH_PYTHON=on -DCMAKE_PREFIX_PATH="../deps_installed/Release/glog;../deps_installed/Release/protobuf;../deps_installed/Release/websockets;..\deps_installed\OpenSSL-Win64" -DOpenCV_DIR="C:/tools/opencv/build/x64/vc15/lib" -DOPENSSL_INCLUDE_DIRS="..\deps_installed\OpenSSL-Win64\include" ..
 cmake --build . --target install --config Release -j 4
 
+cd ../build_Debug
+cmake -DWITH_OPENCV=on -DWITH_PYTHON=on -DCMAKE_PREFIX_PATH="../deps_installed/Debug/glog;../deps_installed/Debug/protobuf;../deps_installed/Debug/websockets;..\deps_installed\OpenSSL-Win64" -DOpenCV_DIR="C:/tools/opencv/build/x64/vc15/lib" -DOPENSSL_INCLUDE_DIRS="..\deps_installed\OpenSSL-Win64\include" ..
+cmake --build . --target install --config Debug -j 4
+
 #generate developer kit binaries
+cd ..
 mkdir aditof_library
 mkdir -p aditof_library\bin
 mkdir -p aditof_library\lib
 mkdir -p aditof_library\bin\python
-cp -r ../sdk/include aditof_library
-cp sdk\Release\aditof.dll aditof_library\bin
-cp sdk\Release\aditof.lib aditof_library\lib
-cp bindings\python\Release\aditofpython* aditof_library\bin\python
+cp -r sdk\include aditof_library
+
+#copy dll
+cp build_Release\sdk\Release\aditof.dll aditof_library\bin
+cp build_Debug\sdk\Debug\aditof.dll aditof_library\bin\aditofd.dll
+
+#copy lib
+cp build_Release\sdk\Release\aditof.lib aditof_library\lib
+cp build_Debug\sdk\Debug\aditof.lib aditof_library\lib\aditofd.lib
+cp build_Debug\sdk\Debug\aditof.pdb aditof_library\lib\aditofd.pdb
+
+#copy python, openSSL, openCV
+cp build_Release\bindings\python\Release\aditofpython* aditof_library\bin\python
+cp deps_installed\OpenSSL-Win64\bin\libcrypto* aditof_library\bin
+cp deps_installed\OpenSSL-Win64\bin\libssl* aditof_library\bin
 Compress-Archive -Path aditof_library -DestinationPath aditof_x64_vs16.zip
 
 #generate Installer
 SET PATH=packages\Tools.InnoSetup.5.6.1\tools
-iscc ..\cmake\aditof-setup.iss.cmakein
+iscc cmake\aditof-setup.iss.cmakein
 
 #publish artifacts
 Get-ChildItem $env:BUILD_ARTIFACTSTAGINGDIRECTORY -Force -Recurse | Remove-Item -Force -Recurse
--- ci/azure/windows-build/install_deps.ps1
+++ ci/azure/windows-build/install_deps.ps1
@@ -1,36 +1,61 @@
 $ARCH=$Env:ARCH
 $GENERATOR=$Env:COMPILER
 
-cinst opencv --version 3.4.1
 cinst openssl
 
 $local_path=$pwd
 mkdir deps_installed
-mkdir -p deps_installed/glog
-mkdir -p deps_installed/protobuf 
-mkdir -p deps_installed/websockets
-cp -r C:/'Program Files'/OpenSSL-Win64 deps_installed
-cp -r C:/tools/opencv deps_installed
 
+mkdir -p deps_installed/Release/glog
+mkdir -p deps_installed/Debug/glog
+
+mkdir -p deps_installed/Release/protobuf
+mkdir -p deps_installed/Debug/protobuf 
+
+mkdir -p deps_installed/Release/websockets
+mkdir -p deps_installed/Debug/websockets
+
+cp -r C:/'Program Files'/OpenSSL-Win64 deps_installed
+
+#Install glog
 git clone --branch v0.3.5 --depth 1 https://github.com/google/glog
 cd glog
-mkdir build_0_3_5 
-cd build_0_3_5
-cmake -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/glog" -G $GENERATOR ..
+mkdir build_0_3_5_Release
+mkdir build_0_3_5_Debug
+
+cd build_0_3_5_Release
+cmake -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Release/glog" -G $GENERATOR ..
 cmake --build . --target install --config Release -j 4
 
+cd ../build_0_3_5_Debug
+cmake -DWITH_GFLAGS=off -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Debug/glog" -G $GENERATOR ..
+cmake --build . --target install --config Debug -j 4
+
+#Install websockets
 cd $local_path
 git clone --branch v3.2.3 --depth 1 https://github.com/warmcat/libwebsockets
 cd libwebsockets
-mkdir build_3_2_3
-cd build_3_2_3
-cmake -DOPENSSL_ROOT_DIR="$local_path/deps_installed/OpenSSL-Win64" -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/websockets" -G $GENERATOR ..
+mkdir build_3_2_3_Release
+mkdir build_3_2_3_Debug
+
+cd build_3_2_3_Release
+cmake -DOPENSSL_ROOT_DIR="$local_path/deps_installed/OpenSSL-Win64" -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Release/websockets" -G $GENERATOR ..
 cmake --build . --target install --config Release -j 4 
+
+cd ../build_3_2_3_Debug
+cmake -DOPENSSL_ROOT_DIR="$local_path/deps_installed/OpenSSL-Win64" -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Debug/websockets" -G $GENERATOR ..
+cmake --build . --target install --config Debug -j 4
 
 cd $local_path
 git clone --branch v3.9.0 --depth 1 https://github.com/protocolbuffers/protobuf
 cd protobuf
-mkdir build_3_9_0 
-cd build_3_9_0
-cmake -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/protobuf" -G $GENERATOR ../cmake
+mkdir build_3_9_0_Release
+mkdir build_3_9_0_Debug
+
+cd build_3_9_0_Release
+cmake -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Release/protobuf" -G $GENERATOR ../cmake
 cmake --build . --target install --config Release -j 4
+
+cd ../build_3_9_0_Debug
+cmake -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -DCMAKE_INSTALL_PREFIX="$local_path/deps_installed/Debug/protobuf" -G $GENERATOR ../cmake
+cmake --build . --target install --config Debug -j 4
