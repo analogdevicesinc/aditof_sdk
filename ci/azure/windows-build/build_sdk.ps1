6d27104fc2a27d0958ebb972f280d3df3b755bc9
SeptimiuVana <Vana.Septimiu@analog.com>
2022-01-27 13:49:58 +0200

    ci/azure: generate windows installer
    Signed-off-by: SeptimiuVana <Vana.Septimiu@analog.com>

--- ci/azure/windows-build/build_sdk.ps1
+++ ci/azure/windows-build/build_sdk.ps1
@@ -6,3 +6,11 @@ mkdir build
 cd build
 cmake -DCMAKE_PREFIX_PATH="../deps_installed/glog;../deps_installed/protobuf;../deps_installed/websockets;..\deps_installed\OpenSSL-Win64" -DOpenCV_DIR="C:/tools/opencv/build/x64/vc15/lib" -DOPENSSL_INCLUDE_DIRS="..\deps_installed\OpenSSL-Win64\include" ..
 cmake --build . --target install --config Release -j 4
+
+#generate Installer
+SET PATH=packages\Tools.InnoSetup.5.6.1\tools
+iscc ..\cmake\aditof-setup.iss.cmakein
+cd C:/
+ls
+Get-ChildItem $env:BUILD_ARTIFACTSTAGINGDIRECTORY -Force -Recurse | Remove-Item -Force -Recurse
+cp aditof-setup.exe $env:BUILD_ARTIFACTSTAGINGDIRECTORY
