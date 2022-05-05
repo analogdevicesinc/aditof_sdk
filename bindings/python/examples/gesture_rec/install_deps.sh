bea8c3fa608ee8bda8ae2a682365b1ceeb91d1fd
cristina-suteu <cristina.suteu@analog.com>
2022-05-05 14:12:51 +0300

    python: fix deps script
    Signed-off-by: cristina-suteu <cristina.suteu@analog.com>

--- bindings/python/examples/gesture_rec/install_deps.sh
+++ bindings/python/examples/gesture_rec/install_deps.sh
@@ -4,5 +4,7 @@
 sudo apt-get install python3-tk
 sudo apt-get install python3-matplotlib
 sudo apt-get install python3-skimage
-sudo apt-get install python-ImageTk
+sudo apt-get install -y python-pil.imagetk
 pip3 install Pillow
+cp notebook.py /home/analog/Desktop
+cp process.py /home/analog/Desktop
