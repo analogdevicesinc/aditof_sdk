f85398d7ffe4b7e22f5873d623285eb6eafc1147
cristina-suteu <cristina.suteu@analog.com>
2022-05-05 14:19:44 +0300

    python: hand ex fix deps
    Signed-off-by: cristina-suteu <cristina.suteu@analog.com>

--- bindings/python/examples/gesture_rec/install_deps.sh
+++ bindings/python/examples/gesture_rec/install_deps.sh
@@ -1,10 +1,12 @@
 
 #!/bin/bash
 
-sudo apt-get install python3-tk
-sudo apt-get install python3-matplotlib
-sudo apt-get install python3-skimage
-sudo apt-get install -y python-pil.imagetk
-pip3 install Pillow
+sudo apt-get install -y python3-tk
+sudo apt-get install -y python3-matplotlib
+sudo apt-get install -y python3-skimage
+sudo apt-get install -y python3-pil.imagetk
+
 cp notebook.py /home/analog/Desktop
 cp process.py /home/analog/Desktop
+
+pip3 install Pillow
