74645a159b2dc35699b768e0a8a78597d6859141
cristina-suteu <cristina.suteu@analog.com>
2022-05-05 14:33:15 +0300

    python fix deps example
    Signed-off-by: cristina-suteu <cristina.suteu@analog.com>

--- bindings/python/examples/gesture_rec/install_deps.sh
+++ bindings/python/examples/gesture_rec/install_deps.sh
@@ -5,8 +5,7 @@ sudo apt-get install -y python3-tk
 sudo apt-get install -y python3-matplotlib
 sudo apt-get install -y python3-skimage
 sudo apt-get install -y python3-pil.imagetk
-
+sudo apt-get install -y python3-pil
 cp notebook.py /home/analog/Desktop
 cp process.py /home/analog/Desktop
 
-pip3 install Pillow
