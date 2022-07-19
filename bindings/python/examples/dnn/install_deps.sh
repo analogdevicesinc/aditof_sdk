51226d7b5d1822db8d39f77f64675f3f7bb274be
cristina-suteu <cristina.suteu@analog.com>
2022-07-19 16:00:34 +0300

    fix deps script
    Signed-off-by: cristina-suteu <cristina.suteu@analog.com>

--- bindings/python/examples/dnn/install_deps.sh
+++ bindings/python/examples/dnn/install_deps.sh
@@ -8,12 +8,10 @@ sudo sudo pip3 install opencv-contrib-python
 
 echo "Building SDK with Python bindings"
 
-cd ~/Workspace/aditof_sdk/build
-cmake -DWITH_PYTHON=ON ../
+cd ~/Workspace/aditof_sdk/bindings/python/examples/dnn
+cmake .
 
 echo "Copying files to desktop"
-mkdir ~/Desktop/dnn
-cp ~/Workspace/aditof_sdk/bindidngs/python/examples/dnn/MobileNetSSD_deploy.prototxt ~/Desktop/dnn
-cp ~/Workspace/aditof_sdk/bindidngs/python/examples/dnn/MobileNetSSD_deploy.caffemodel ~/Desktop/dnn
-cp ~/Workspace/aditof_sdk/bindidngs/python/examples/dnn/dnn.py ~/Desktop/dnn
-cp ~/Workspace/aditof_sdk/build/bindidngs/python/aditofpython.cpython-36m-aarch64-linux-gnu.so ~/Desktop/dnn
+cp ~/Workspace/aditof_sdk/bindidngs/python/examples/dnn/MobileNetSSD_deploy.prototxt ~/Desktop
+cp ~/Workspace/aditof_sdk/bindidngs/python/examples/dnn/MobileNetSSD_deploy.caffemodel ~/Desktop
+cp ~/Workspace/aditof_sdk/bindidngs/python/examples/dnn/dnn.py ~/Desktop
