305db1ba46160e860c8539cb65e7911d8c813e59
Alexandru Lie <Alexandru.Lie@analog.com>
2023-08-25 20:44:49 +0300

    Added depth sensor detection for fxtof
    Signed-off-by: Alexandru Lie <Alexandru.Lie@analog.com>

--- sdk/src/cameras/ad-fxtof1-ebz/raspberrypi/sensor_enumerator_raspberrypi.cpp
+++ sdk/src/cameras/ad-fxtof1-ebz/raspberrypi/sensor_enumerator_raspberrypi.cpp
@@ -48,19 +48,150 @@
 
 using namespace aditof;
 
-Status TargetSensorEnumerator::searchSensors() {
+aditof::Status findDepthSensor(std::string &media,std::string &dev,std::string &subdev){
 
+    char *buf;
+    int size = 0;
+    size_t idx = 0;
+
+    /* Run media-ctl to get the video processing pipes */
+    char cmd[64];
+    sprintf(cmd, "sudo media-ctl -d %s -p", media.c_str());
+    FILE *fp = popen(cmd, "r");
+    if (!fp) {
+        LOG(WARNING) << "Error running media-ctl";
+        return Status::GENERIC_ERROR;
+    }
+
+    /* Read the media-ctl output stream */
+    buf = (char *)malloc(128 * 1024);
+    while (!feof(fp)) {
+        fread(&buf[size], 1, 1, fp);
+        size++;
+    }
+    pclose(fp);
+    buf[size] = '\0';
+
+    /* Get the list of entities from output buffer */
+    std::string str(buf);
+    free(buf);
+    std::vector<std::string> entities;
+    size_t pos = str.find("entity");
+    while (pos != std::string::npos) {
+        size_t pos1 = str.find("entity", pos + 1);
+        if (pos1 != std::string::npos) {
+            std::string str1(str, pos, pos1 - pos);
+            entities.push_back(str1);
+        } else {
+            std::string str1(str, pos, size - pos);
+            entities.push_back(str1);
+            break;
+        }
+        pos = pos1;
+    }
+
+    /* Find the names of the associated device and subdevice */
+    std::string fstr("<- \"addi9036");
+    for (idx = 0; idx < entities.size(); idx++) {
+        size_t found = entities[idx].find(fstr);
+        size_t found1 = 0;
+        if (found != std::string::npos) {
+            found = entities[idx].find("Source", found);
+            if (found == std::string::npos) {
+                found = entities[idx].find("device node name ");
+                if (found == std::string::npos) {
+                    break;
+                }
+                found1 = entities[idx].find("pad", found);
+                if (found1 == std::string::npos) {
+                    break;
+                }
+                dev = entities[idx].substr(
+                    found + strlen("device node name "),
+                    found1 - found - strlen("device node name ") - 2);
+                break;
+            }
+            found = entities[idx].find("ENABLED", found);
+            if (found != std::string::npos) {
+                found = entities[idx].rfind("\"", found);
+                found1 = entities[idx].rfind("\"", found - 1);
+                std::string str1(entities[idx], found1, found - found1);
+                fstr = "<- " + str1;
+                continue;
+            }
+        }
+    }
+    fstr = "addi9036";
+    for (; idx < entities.size(); idx++) {
+        size_t found = entities[idx].find(fstr);
+        size_t found1 = 0;
+        if (found == std::string::npos) {
+            continue;
+        }
+
+        found = entities[idx].find("device node name ");
+        if (found == std::string::npos) {
+            break;
+        }
+        found1 = entities[idx].find("pad", found);
+        if (found1 == std::string::npos) {
+            break;
+        }
+        subdev = entities[idx].substr(found + strlen("device node name "),
+                                           found1 - found -
+                                               strlen("device node name ") - 2);
+        break;
+    }
+
+    return Status::OK;
+}
+
+Status TargetSensorEnumerator::searchSensors() {
+    Status status = Status::ok;
     LOG(INFO) << "Looking for sensors on the target: Raspberry PI";
 
-    // TO DO: Don't guess the device, find a way to identify it so we are sure
-    // we've got the right sensor and it's compatible with the SDK
-    SensorInfo sInfo;
-    sInfo.sensorType = SensorType::SENSOR_ADDI9036;
-    sInfo.driverPath = "/dev/video0";
-    sInfo.subDevPath = "/dev/video0";
-    sInfo.captureDev = CAPTURE_DEVICE_NAME;
-    m_sensorsInfo.emplace_back(sInfo);
+    // Find all media device paths
+    std::vector<std::string> mediaPaths;
+    const std::string mediaDirPath("/dev/");
+    const std::string mediaBaseName("media");
 
+    DIR *dirp = opendir(mediaDirPath.c_str());
+    struct dirent *dp;
+    while ((dp = readdir(dirp))) {
+        if (!strncmp(dp->d_name, mediaBaseName.c_str(),
+                     mediaBaseName.length())) {
+            std::string fullMediaPath = mediaDirPath + std::string(dp->d_name);
+            mediaPaths.emplace_back(fullMediaPath);
+        }
+    }
+    closedir(dirp);
+
+    // Identify any eligible time of flight cameras
+    for (std::string media : mediaPaths) {
+        DLOG(INFO) << "Looking at: " << media << " for an eligible TOF camera";
+
+        std::string devPath;
+        std::string subdevPath;
+
+        status = findDepthSensor(media, devPath, subdevPath);
+        if (status != Status::OK) {
+            LOG(WARNING) << "failed to find device paths at media: " << media;
+            return status;
+        }
+
+        if (devPath.empty() || subdevPath.empty()) {
+            continue;
+        }
+        DLOG(INFO) << "Considering: " << media << " an eligible TOF camera";
+
+        SensorInfo sInfo;
+        sInfo.sensorType = SensorType::SENSOR_ADDI9036;
+        sInfo.driverPath = devPath;
+        sInfo.subDevPath = subdevPath;
+        sInfo.captureDev = CAPTURE_DEVICE_NAME;
+        m_sensorsInfo.emplace_back(sInfo);
+    }
+    
     StorageInfo eepromInfo;
     eepromInfo.driverName = EEPROM_NAME;
     eepromInfo.driverPath = EEPROM_DEV_PATH;
